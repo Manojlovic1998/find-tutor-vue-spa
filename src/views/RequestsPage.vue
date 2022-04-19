22ac8fd4179d97cb16cd0c56da4250eef48091f6
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-19 17:13:54 +0200

    Add base context to Requests view

--- src/views/RequestsPage.vue
+++ src/views/RequestsPage.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Requests</h1>
+  <BaseHero>
+    <div class="hero-jumb mx-auto">
+      <div class="d-flex flex-column h-100 justify-content-center">
+        <h1 class="fw-bold text-center">Requests Received</h1>
+      </div>
+    </div>
+  </BaseHero>
 </template>
+
+<script>
+import BaseHero from "../components/UI/BaseHero.vue";
+
+export default {
+  components: {
+    BaseHero,
+  },
+};
+</script>
+
+<style scoped>
+.hero-jumb {
+  max-width: 450px;
+  width: 100%;
+  border-radius: 8px;
+  height: 157px;
+}
+</style>
