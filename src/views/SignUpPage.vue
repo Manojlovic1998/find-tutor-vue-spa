b315557f0e17f814fcc907c4cd4d2e4d1ae0c391
Nemanja <manojlovic911@hotmail.com>
2022-04-21 20:38:19 +0200

    Update context layer of SignUp view with form, submit and v-model methods

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,7 +1,92 @@
 <template>
-  <p>This Is a SignUp Page</p>
+  <BaseMain>
+    <BaseHero>
+      <div class="col-8 col-md-5 col-lg-4 mx-auto mt-5">
+        <h1 class="text-center">Sign Up</h1>
+        <p class="fw-bold text-gray mt-3 text-center" v-if="mode === 'login'">
+          You don't have an account yet?
+          <router-link @click="switchMode()" to="/signup">Register</router-link>
+        </p>
+        <p class="fw-bold text-gray mt-3 text-center" v-else>
+          You already have an account?
+          <router-link @click="switchMode()" to="/signup">LogIn</router-link>
+        </p>
+        <form @submit.prevent="onSubmit" class="mt-5">
+          <p class="text-center text-danger fw-bold" v-if="!formValidStatus">
+            Please make sure that you have entered a valid email and password.
+          </p>
+          <div class="form-group">
+            <label for="email" class="form-label">Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              name="email"
+              placeholder="example@mail.com"
+              v-model.trim="email"
+              required
+            />
+          </div>
+          <div class="form-group mt-3">
+            <label for="password" class="form-label">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              name="password"
+              v-model.trim="password"
+              required
+            />
+          </div>
+          <BaseButton class="mt-4" type="submit">{{
+            mode === "login" ? "LogIn" : "Register"
+          }}</BaseButton>
+        </form>
+      </div>
+    </BaseHero>
+  </BaseMain>
 </template>
 
 <script>
-export default {};
+import BaseMain from "../components/UI/BaseMain.vue";
+import BaseHero from "../components/UI/BaseHero.vue";
+import BaseButton from "../components/UI/BaseButton.vue";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      formValidStatus: true,
+      mode: "login",
+    };
+  },
+  components: {
+    BaseMain,
+    BaseHero,
+    BaseButton,
+  },
+  methods: {
+    onSubmit() {
+      this.formValidStatus = true;
+      if (
+        this.password.length < 6 ||
+        !this.email.includes("@") ||
+        this.email === ""
+      ) {
+        this.formValidStatus = false;
+        return;
+      }
+
+      this.formValidStatus = true;
+    },
+    switchMode() {
+      if (this.mode === "login") {
+        this.mode = "register";
+      } else {
+        this.mode = "login";
+      }
+    },
+  },
+};
 </script>
