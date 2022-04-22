9d98b2d26c6b188cda7bf637af80dcecaebeef76
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-23 01:48:56 +0200

    Add tutors store module to main store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 import actions from "./actions";
 import getters from "./getters";
 import auth from "./modules/auth/index";
+import tutors from "./modules/tutors/index";
 import mutations from "./mutations";
 
 const store = createStore({
   modules: {
     auth,
+    tutors,
   },
   state() {
     return {
