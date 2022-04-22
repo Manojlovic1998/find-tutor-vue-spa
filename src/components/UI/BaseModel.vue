e51cf9685b05d4b22ef6e9f29d577eb51ed10cee
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-22 17:53:25 +0200

    Add modal and its overlay comp

---
+++ src/components/UI/BaseModel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="message-modal bg-white d-inline-flex flex-column">
+    <div class="w-100 mt-3">
+      <slot name="title"></slot>
+    </div>
+    <hr />
+    <div :class="[bodyStyle]">
+      <slot name="body"></slot>
+    </div>
+    <br />
+    <div class="mt-auto d-inline ms-auto">
+      <slot name="actions"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bodyStyle: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.message-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  z-index: 1;
+  transform: translate(-50%, -75%);
+  min-width: 200px;
+  max-width: 400px;
+  width: 100%;
+  min-height: 250px;
+  border-radius: 8px;
+  filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.25));
+  padding: 13px 21px;
+}
+</style>
---
+++ src/components/UI/BaseOverlay.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="overlay"></div>
+</template>
+
+<style scoped>
+.overlay {
+  background-color: hsla(0, 0%, 0%, 0.4);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
