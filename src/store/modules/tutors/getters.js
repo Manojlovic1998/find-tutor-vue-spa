225bed6f0eb007d1d6f591b946bbfb8f775622af
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-25 03:20:12 +0200

    Create getter for checking if tutors data is empty

--- src/store/modules/tutors/getters.js
+++ src/store/modules/tutors/getters.js
@@ -2,4 +2,8 @@ export default {
   getTutorsData(state) {
     return state.tutorsList;
   },
+  isTutorsDataEmpty(state) {
+    const tutorDataState = Object.keys(state.tutorsList).length === 0;
+    return tutorDataState;
+  },
 };
